dbe78daf3aeecc383595ff5b11cd4215c79ba3b9
Leonardo Ariel Sánchez <81994197+Leo-git056@users.noreply.github.com>
2026-03-02 17:54:44 -0300

    Add files via upload

---
+++ AutoBT/build/default/debug/Autito.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Autito.o:  \
+	D:/DOCUMENTOS/Mis\ Documentos/Leo/Digitales\ II/Guía\ de\ Ejercicios\ MPLAB/AutoBT.X/Autito.asm  \
+
---
+++ AutoBT/build/default/production/Autito.o.d
@@ -0,0 +1,3 @@
+build/default/production/Autito.o:  \
+	D:/DOCUMENTOS/Mis\ Documentos/Leo/Digitales\ II/Guía\ de\ Ejercicios\ MPLAB/AutoBT.X/Autito.asm  \
+
